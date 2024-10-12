db3e8ba8cec30662732f8aef9bd0f15f0aeedeb1
Andressa Dias <admarin73@gmail.com>
2024-10-12 12:50:11 -0300

    Adicionar arquivos de projeto.

---
+++ reciclamais-backend/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace reciclamais_backend.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
